b3bcdd67d4c74531549a65035542307aad069b65
Chenlong Wang <wcloong@hust.edu.cn>
2025-07-31 23:51:32 +0800

    Delete DataProcessor directory

--- DataProcessor/__init__.py
+++
@@ -1,7 +0,0 @@
-from .api_update import *
-from .api_detector import *
-from .repo_crawler import *
-from .jsonl_switch import *
-from .inspect_signature import *
-from .jsonl_switch import *
-from .signature_mapping import *
--- DataProcessor/venv/readme.txt
+++
@@ -1,1 +0,0 @@
-Virtual environment saving directory
